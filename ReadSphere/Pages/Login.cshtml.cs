3ea30d0fcaf8b8e4a1b59b9c27eac60c6175cc8f
Abdullah Ayman <abdulluhayman@gmail.com>
2024-11-24 17:52:44 +0200

    Added the cookies and sign in

--- ReadSphere/Pages/Login.cshtml.cs
+++ ReadSphere/Pages/Login.cshtml.cs
@@ -2,18 +2,19 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
 
 public class Login : PageModel
 {
     // Try to put BindProperty with everything 
     [BindProperty]
-    public string Age { get; set; }
+    public string? Age { get; set; }
     [BindProperty]
 
-    public string Email { get; set; }
+    public string? Email { get; set; }
     [BindProperty]
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     public void OnGet()
     {
@@ -21,6 +22,16 @@ public class Login : PageModel
 
     public IActionResult OnPost()
     {   //Pass this attribute to the Index Model
+        // Create a cookie
+        Response.Cookies.Append(
+                    "User",
+                    Name,
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.Now.AddHours(1),
+                        HttpOnly = true,
+                        Secure = true
+                    });
 
         return RedirectToPage("Index", new { name = Name, age = Age });
     }
